2a8ee516d26bed0d2738fa83c90505360217bd77
FreedSoul <mattwork07@gmail.com>
2024-05-26 13:33:38 -0500

    first commit-django+tailwind

---
+++ chatandworkApp/chatandworkApp/urls.py
@@ -0,0 +1,12 @@
+"""
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('chat.urls')),
+    path('__reload__/', include('django_browser_reload.urls'))
+]
